1af6aab55c4b1ff552bcb91612b97df514e3a819
Tomoaki Imai <tomoima525@gmail.com>
2022-07-07 22:37:24 -0700

    A simple contract that triggers event

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Contract.sol";
 
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        new Contract();
+        vm.stopBroadcast();
     }
 }
